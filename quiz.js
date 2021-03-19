74354ab3832fc6b7df922e723b440fb00529b77f
D-Savel <dad.savel@gmail.com>
2021-03-19 17:24:42 +0100

    Resolution Exercices guessnumber quiz

--- quiz.js
+++ quiz.js
@@ -1,7 +1,20 @@
 const readlineSync = require('readline-sync')
-let answerTab = {
-  Question1: 1, Question2: 1, Question3: 2, Question4: 2, Question5: 1
-}
+let questionnaire = [["1", "Le C++ est un :", "Language", "Compilateur", 1],
+["2", "TypeScript est une évolution de Javascript", "Vrai", "Faux", 1],
+["3", "Lire les cours avant de faire les exercices est inutile", "Vrai", "Faux", 2],
+["4", "React.js a été developpé par Google", "Vrai", "Faux", 2],
+["5", "Ethereum est une blockchain publique", "Vrai", "Faux", 1]
+]
 
-const inputAnswer1 = readlineSync.question("Question 1 : le C++ est un :\n1: Language\n2: Compilateur\nEntrez votre réponse")
-const inputAnswer2 = readlineSync.question("Question 2: TypeScript est une évolution de Javascript: \n1: Vrai\n2: Faux")
+let score = 0
+
+for (elem of questionnaire) {
+  do {
+    inputAnswer = readlineSync.question(`Question${elem[0]} : ${elem[1]}\n1: ${elem[2]}\n2: ${elem[3]}\nVotre réponse? `)
+  }
+  while (inputAnswer !== '1' && inputAnswer !== '2')
+  if (Number(inputAnswer) === elem[4])
+    score++
+  inputAnswer = 'new'
+}
+console.log(`Vous avez : ${score} bonne${score > 1 ? 's' : ''} réponse${score > 1 ? 's' : ''}`)
